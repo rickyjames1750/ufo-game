ec6f97ab16e64126a997d18f51779404ecfdec1e
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-27 22:40:16 -0800

    Added Game Title W/ Instructions
    Added Game Title W/ Instructions

--- ufo_functions.cpp
+++ ufo_functions.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 
 // Define functions
+void greet() {
+  std::cout << "=============\n";
+  std::cout << "UFO: The Game\n";
+  std::cout << "=============\n";
+  std::cout << "Instructions: save your friend from alien abduction by guessing the letters in the codeword.\n";
+}
 
 
 
